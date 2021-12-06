9dfb83fd7b47869117bfdcced152ab123a1ccfbf
mohanpan <echoiepan@gmail.com>
2021-12-06 16:18:04 -0500

    setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 
 import './App.css';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+import { Navbar } from './components/Navbar';
+import { SubaruHomePage } from './components/pages/SubaruHomePage';
+import { ShoppingCartPage } from './components/pages/ShoppingCartPage';
+import { AccessoryDetailsPage } from './components/pages/AccessoryDetailsPage';
+import { LoginPage } from './components/pages/LoginPage';
+
 function App() {
   return (
     <div className="App">
-      <h1>Hi</h1>
+      <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <SubaruHomePage />
+        </Route>
+        <Route path="/cart">
+           <ShoppingCartPage />
+        </Route>
+        <Route path="/accessory/:id">
+          <AccessoryDetailsPage />
+        </Route>
+        <Route path="/login">
+          <LoginPage />
+        </Route>
+      </Switch>
+    </Router>
     </div>
   );
 }
---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,22 @@
+import {
+    NavLink
+} from "react-router-dom";
+
+import { FaStore, FaShoppingCart} from 'react-icons/fa';
+
+import "./styles.css"
+
+export const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <ul className="navbar-list">
+                <li>
+                    <NavLink exact={true} activeClassName="nav-selected" to="/">Accessories</NavLink>
+                </li>
+                <li>
+                    <NavLink activeClassName="nav-selected" to="/cart"> My Cart</NavLink>
+                </li>
+            </ul>
+        </nav>
+    )
+}
